96962a8098c41bb9120d0fc7ce21293f88753e34
uFNet1 <91597674+uFNet1@users.noreply.github.com>
2025-06-07 10:15:52 +0300

    get user choice and play round logic

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const random = Math.random();
 
@@ -5,3 +8,28 @@ function getComputerChoice() {
   else if (random > 0.3 && random < 0.6) return "paper";
   else if (random >= 0.6) return "scissors";
 }
+function getUserChoice() {
+  const user = prompt("Type your answer");
+  return user;
+}
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = String(humanChoice).toLowerCase();
+
+  if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log("You lose! Paper beats Rock");
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    console.log("You lose! Scissors beats Paper");
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    console.log("You lose! Rock beats Scissors");
+  } else if (humanChoice === computerChoice) {
+    console.log("Tie!");
+  } else {
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+  }
+}
+
+const humanSelection = getUserChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
